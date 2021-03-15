03ac2acbba494617f1395659b4b4f09ef379442b
Wataru Ono <wtr0414@gmail.com>
2021-03-15 22:17:15 +0900

    implement simple countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Countdown from "react-countdown";
 
 const Index = () => {
-  return <div>Index</div>
+  const [count, setCount] = useState<number>(Date.now() + 5000)
+
+  const resetCount = () => {
+    setCount(Date.now() + 5000)
+  }
+
+  const renderer = ({ hours, minutes, seconds, completed }: any) => {
+    if (completed) {
+      return () => setCount(Date.now() + 5000)
+    } else {
+      return <span>{hours}:{minutes}:{seconds}</span>
+    }
+  }
+
+  return (
+    <Countdown
+      date={count}
+      onComplete={resetCount}
+      overtime={true}
+      renderer={renderer}
+    />
+  )
 };
 
 export default Index;
