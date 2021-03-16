5dbe1e621d1e882a5c5f4c6e528405c488a5b735
Wataru Ono <wtr0414@gmail.com>
2021-03-16 20:21:43 +0900

    create different type stamina

---
+++ src/components/UnusedStamina.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+const UnusedStamina: FC = () => {
+  return (
+    <div className={"rounded-full h-24 w-24 border-2 mx-2 bg-indigo-600"} />
+  );
+};
+
+export default UnusedStamina;
--- src/components/UsedStamina.tsx
+++ src/components/UsedStamina.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from "react";
 
-const Stamina: FC = () => {
+const UsedStamina: FC = () => {
   return (
     <div className={"rounded-full border-black h-24 w-24 border-2 mx-2"} />
   );
 };
 
-export default Stamina;
+export default UsedStamina;
