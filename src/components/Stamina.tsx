4f8a894b9f376bd52a68cb3a78e31cfe317b3153
Wataru Ono <wtr0414@gmail.com>
2021-03-16 19:22:23 +0900

    implement stamina atom

---
+++ src/components/Stamina.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+const Stamina: FC = () => {
+  return (
+    <div className={"rounded-full border-black h-24 w-24 border-2"} />
+  );
+};
+
+export default Stamina;
